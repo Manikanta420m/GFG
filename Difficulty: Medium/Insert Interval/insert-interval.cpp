4b8c046fd0e451086009fefb23367d5c48475dba
ManiKanta <manikanta420m@gmail.com>
2025-08-15 22:12:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Insert Interval/insert-interval.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    vector<vector<int>> insertInterval(vector<vector<int>> &intervals,
+                                       vector<int> &newInterval) {
+        
+        
+        intervals.push_back({newInterval[0],newInterval[1]});
+        vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        int n = intervals.size();
+        ans.push_back(intervals[0]);
+        
+        for(int i=1; i<n; i++){
+            int end = ans.back()[1];
+            int st = intervals[i][0];
+            
+            if(end >= st){
+                ans.back()[1] = max(end,intervals[i][1]);
+            }else{
+                
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
+
