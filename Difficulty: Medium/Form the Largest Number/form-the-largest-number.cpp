ea150c613acbaa6501ee1b4ba83c75c795b52c7f
ManiKanta <manikanta420m@gmail.com>
2025-08-16 08:42:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Form the Largest Number/form-the-largest-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+  
+    static bool comp(string first,string second){
+        return (first+second<second+first);
+    }
+  
+    string findLargest(vector<int> &arr) {
+        vector<string>v;
+        for(auto i:arr)v.push_back(to_string(i));
+        sort(v.rbegin(),v.rend(),comp);
+        string s="";
+        for(auto i:v){s+=i;}
+        return (s[0]=='0'?"0":s);
+    }
+};
