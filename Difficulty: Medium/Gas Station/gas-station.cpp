0d337d7e1b35e4381f8be29c2eab1411af15e169
ManiKanta <manikanta420m@gmail.com>
2025-09-15 16:25:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.cpp
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -1,35 +1,27 @@
 class Solution {
   public:
-    int startStation(vector<int> &gas, vector<int> &cost) {
-        //  code here
-        vector<int>final;
-        int total=0;
-        for(int i=0;i<gas.size();i++) {
-            final.push_back(gas[i]-cost[i]);
-            total+=final[i];
-        }
-        if(total<0) {
-            return -1;
-        }
-        vector<int>postFix;
-        
-        int curr=0;
-        for(int i=final.size()-1;i>=0;i--) {
-            curr+=final[i];
-            postFix.push_back(curr);
-        }
-        
-        reverse(postFix.begin(),postFix.end());
-        
-        int idx=0;
-        int maxi=INT_MIN;
-        for(int i=0;i<postFix.size();i++) {
-            if(postFix[i]>maxi) {
-                idx=i;
-                maxi=postFix[i];
+    int startStation(vector<int> &gas, vector<int> &cost) 
+    {
+        int n = gas.size();
+        int ans = -1;
+        int total_gas = accumulate(gas.begin(), gas.end(), 0);
+        int total_cost = accumulate(cost.begin(), cost.end(), 0);
+        if(total_gas < total_cost)
+            return ans;
+        int gas_avail = 0;
+        int start = 0;
+        for(int i = 0; i < n; i++)
+        {
+            gas_avail += gas[i] - cost[i];
+            if(gas_avail < 0)
+            {
+                start = i+1;
+                gas_avail = 0;
             }
+            
         }
-        return idx;
+        return start;
+        
+        
     }
 };
-
