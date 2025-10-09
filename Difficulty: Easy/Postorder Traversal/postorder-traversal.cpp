8132114fcaaccb13901293b3fa273bc03efc888e
ManiKanta <manikanta420m@gmail.com>
2025-10-09 06:25:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Postorder Traversal/postorder-traversal.cpp
@@ -0,0 +1,32 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  private:
+    void solve(Node *root,vector<int>&ans){
+        if(!root)return ;
+        solve(root->left,ans);
+        solve(root->right,ans);
+        ans.push_back(root->data);
+        return;
+    }
+  public:
+    vector<int> postOrder(Node* root) {
+        // code here
+        vector<int>ans;
+        solve(root,ans);
+        return ans;
+    }
+};
