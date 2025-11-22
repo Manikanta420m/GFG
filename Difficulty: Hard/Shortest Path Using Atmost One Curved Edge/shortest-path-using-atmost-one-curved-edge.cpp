6a8ae77a33806f0b3ee9c1884fad92e5ad8d7ee1
ManiKanta <manikanta420m@gmail.com>
2025-11-22 09:04:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Shortest Path Using Atmost One Curved Edge/shortest-path-using-atmost-one-curved-edge.cpp
+++ Difficulty: Hard/Shortest Path Using Atmost One Curved Edge/shortest-path-using-atmost-one-curved-edge.cpp
@@ -1,61 +1,51 @@
 class Solution {
-public:
-    const long long INF = 1e18;
-
-    vector<long long> dijkstra(int V, int src, vector<vector<pair<int,int>>> &adj) {
-        vector<long long> dist(V, INF);
-        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> pq;
-
-        dist[src] = 0;
-        pq.push({0, src});
-
-        while(!pq.empty()) {
-            auto [d, u] = pq.top(); pq.pop();
-            if(d != dist[u]) continue;
-
-            for(auto &p : adj[u]) {
-                int v = p.first;
-                int w = p.second;
-
-                if(dist[v] > d + w) {
-                    dist[v] = d + w;
-                    pq.push({dist[v], v});
+  public :
+    void dijstra(int src,vector<int>&dist,vector<pair<int,int>>adj[]){
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,src});
+        dist[src]=0;
+        while(!pq.empty()){
+            auto it=pq.top();
+            pq.pop();
+            if (it.first > dist[it.second]) continue;
+            for(auto i:adj[it.second]){
+                if(it.first+i.second<dist[i.first]){
+                    dist[i.first]=it.first+i.second;
+                    pq.push({dist[i.first],i.first});
                 }
             }
         }
-        return dist;
     }
-
+  public:
     int shortestPath(int V, int a, int b, vector<vector<int>> &edges) {
-        // Build adjacency list with ONLY straight edges
-        vector<vector<pair<int,int>>> adj(V);
-
-        for(auto &e : edges) {
-            int x = e[0], y = e[1], w1 = e[2];
-            adj[x].push_back({y, w1});
-            adj[y].push_back({x, w1});
+        // code here
+        vector<pair<int,int>>adj[V];
+        vector<pair<pair<int,int>,int>>cur;
+        for(auto i:edges){
+            adj[i[0]].push_back({i[1],i[2]});
+            adj[i[1]].push_back({i[0],i[2]});
+            cur.push_back({{i[0],i[1]},i[3]});
         }
-
-        // Run Dijkstra from a and b (using straight edges only)
-        vector<long long> distA = dijkstra(V, a, adj);
-        vector<long long> distB = dijkstra(V, b, adj);
-
-        long long ans = distA[b];   // path without curved edges
-
-        // Try using each curved edge once
-        for(auto &e : edges) {
-            int x = e[0], y = e[1], w2 = e[3];
-
-            // a -> x -> curved -> y -> b
-            if(distA[x] != INF && distB[y] != INF)
-                ans = min(ans, distA[x] + w2 + distB[y]);
-
-            // a -> y -> curved -> x -> b
-            if(distA[y] != INF && distB[x] != INF)
-                ans = min(ans, distA[y] + w2 + distB[x]);
+        vector<int>dist1(V,INT_MAX);
+        vector<int>dist2(V,INT_MAX);
+        dijstra(a,dist1,adj);
+        dijstra(b,dist2,adj);
+        int ans=dist1[b];
+        for(auto i:cur){
+            int a=i.first.first;
+            int b=i.first.second;
+            int wt=i.second;
+            int a1=dist1[a];
+            int b1=dist1[b];
+            int a2=dist2[a];
+            int b2=dist2[b];
+            if(a1!=INT_MAX and b2!=INT_MAX){
+                ans=min(ans,a1+b2+wt);
+            }
+            if(a2!=INT_MAX and b1!=INT_MAX){
+                ans=min(ans,a2+b1+wt);
+            }
         }
-
-        if(ans >= INF) return -1;
-        return (int)ans;
+        return ans==INT_MAX?-1:ans;
     }
 };
