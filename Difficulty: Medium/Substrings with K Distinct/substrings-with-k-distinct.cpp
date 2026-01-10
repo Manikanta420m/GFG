12c3c34680ce074881ed66b66fe00eea123594ec
ManiKanta <manikanta420m@gmail.com>
2026-01-10 17:17:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    int f(string &s,int k){
+        vector<int> c(26,0);
+        int i =0;
+        int j =0;
+        int cnt =0;
+        int ccnt =0;
+        int n =s.size();
+        while(j < n){
+            if(c[s[j]-'a']){
+                c[s[j]-'a']++;
+            }else{
+                c[s[j]-'a']++;
+                ccnt++;
+            }
+            while(i <= j && ccnt == k){
+                c[s[i]-'a']--;
+                if(c[s[i]-'a'] == 0){
+                    ccnt--;
+                }
+                i++;
+                cnt+=(n-j);
+            }
+            j++;
+        }
+        return cnt;
+    }
+    int countSubstr(string& s, int k) {
+        // code here
+        return f(s,k)-f(s,k+1);
+    }
+};
