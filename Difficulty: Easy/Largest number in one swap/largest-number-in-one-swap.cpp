48b1e041d9d93b647a5c37719c312e5e5255ef84
ManiKanta <manikanta420m@gmail.com>
2026-03-09 22:18:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.cpp
+++ Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.cpp
@@ -2,16 +2,31 @@ class Solution {
   public:
     string largestSwap(string &s) {
         // code here
-        string s1=s;
-        sort(s1.rbegin(),s1.rend());
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=s1[i]){
-                for(int j=s.size()-1;j>i;j--){
-                    if(s[j]==s1[i]){
-                        swap(s[i],s[j]);
-                        return s;
-                    }
-                }
+         int n  = s.size();
+        vector<int> maxRight(n,-1); // contains the indexes
+        int maxi = -1;
+        int maxIndex =-1;
+        
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            if(s[i]-'0' > maxi)
+            {
+                maxi = s[i]-'0';
+                maxIndex = i;
+                maxRight[i] = maxIndex; // update the index
+            }
+            else
+            {
+                maxRight[i] = maxIndex;
+            }
+        }
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[maxRight[i]] - '0'> (s[i]-'0'))
+            {
+                swap(s[maxRight[i]],s[i]);
+                return s;
             }
         }
         return s;
