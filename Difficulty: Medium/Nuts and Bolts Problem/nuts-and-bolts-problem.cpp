0984368c570a4454ec56a858f616b76b2e2b2d2b
ManiKanta <manikanta420m@gmail.com>
2026-05-10 07:14:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nuts and Bolts Problem/nuts-and-bolts-problem.cpp
@@ -0,0 +1,8 @@
+class Solution {
+  public:
+    void matchPairs(vector<char> &nuts, vector<char> &bolts) {
+        // code here
+        sort(nuts.begin(),nuts.end());
+        sort(bolts.begin(),bolts.end());
+    }
+};
