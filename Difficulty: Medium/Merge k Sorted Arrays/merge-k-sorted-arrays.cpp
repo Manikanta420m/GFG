5b99c8674f6b6d0e0b163c3b941c8cc9ba2ca4dc
ManiKanta <manikanta420m@gmail.com>
2026-06-19 07:35:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> mergeArrays(vector<vector<int>> &mat) {
+        // Code here
+        priority_queue<int,vector<int>,greater<int>>ch;
+        
+        for(int i=0;i<mat.size();i++)
+        {
+            for(int j=0;j<mat[0].size();j++)
+                ch.push(mat[i][j]);
+        }
+        
+        vector<int>v;
+        
+        while(!ch.empty())
+        {
+            //work
+            
+            v.push_back(ch.top());
+            ch.pop();
+        }
+        return v;
+    }
+};
