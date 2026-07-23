c7c38c2605ea7f69cc438a92de3603feaae9ca37
ManiKanta <manikanta420m@gmail.com>
2026-07-23 06:35:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check Preorder of BST/check-preorder-of-bst.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    bool canRepresentBST(vector<int> &arr) {
+        // code here
+        stack<int> st;
+        int root = INT_MIN;
+
+        for (int x : arr) {
+            if (x < root)
+                return false;
+
+            while (!st.empty() && x > st.top()) {
+                root = st.top();
+                st.pop();
+            }
+
+            st.push(x);
+        }
+
+        return true;
+
+    }
+};
